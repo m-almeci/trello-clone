03f0169bba9e02ea89b3a3e484fd32ef6804f57f
Maria Almeciga <malmeciga0104@gmail.com>
2023-10-11 22:13:01 -0500

    Set up log in component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'centered-md': '0 0 8px rgba(0, 0, 0, 0.15), 0 0 6px rgba(0, 0, 0, 0.1)'
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+  theme: {
+    extend: {
+      fontWeight: {
+        'normal': 300,
+        'medium': 400,
+        'bold': 700
+      }
+    }
+  }
 }
 
